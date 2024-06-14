db48e6efe38567c37fa643fa176441bc1b843974
Shaun Prince <suparious@solidrust.net>
2024-06-14 11:37:34 -0700

    bump version to publish new docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='srt-core',
-    version='0.1.9',
+    version='0.1.11',
     packages=find_packages(include=['srt_core', 'srt_core.*']),
     install_requires=[
         'pyyaml',
@@ -24,5 +24,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.11',
 )
